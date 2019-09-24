8b6e5874c40292cf54c128465181c2938eed2dd8
Kristian F Jones <me@kristianjones.xyz>
2019-09-24 13:12:57 -0500

    fix: Fix

--- ClientDL/src/index.ts
+++ ClientDL/src/index.ts
@@ -1,6 +1,5 @@
 import gql from 'graphql-tag';
 import { decompress } from 'iltorb';
-import pEvent from 'p-event'
 import { extract } from 'tar-fs-fixed';
 import { client, createContainer, startContainer, removeContainer } from './Docker';
 
@@ -36,7 +35,6 @@ async function startClientDL(): Promise<void> {
       },
       async complete() {
         tarStream.end();
-        await pEvent(tarStream, 'end')
         console.log('Done');
         await removeContainer(container.id)
         process.exit(0)
